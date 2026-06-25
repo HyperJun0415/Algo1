9156f672a4ddb9d6c62f7ad9d26408c5dc10916b
HyperJun0415 <leejunyan0415@gmail.com>
2026-06-25 13:40:54 +0800

    heap sort step comment fix

--- heap_sort_step.cpp
+++ heap_sort_step.cpp
@@ -5,8 +5,8 @@
 // Tutorial Class: TT9L
 // Trimester: 2610
 // Member_1: 242UC244PB | LEM JOE ERN | lem.joe.ern@student.mmu.edu.my | 0162237965
-// Member_2: ID | NAME | EMAIL | PHONE
-// Member_3: ID | NAME | EMAIL | PHONE
+// Member_2: 251UC250J5 | LEE JUN YAN | lee.jun.yan@student.mmu.edu.my | 0128500415
+// Member_3: 251UC25141 | SHAWN GOH XUN SHEN | shawn.goh.xun@student.mmu.edu.my | 0199906601
 // Member_4: 242UC244KV | KOH HUI WEN | koh.hui.wen@student.mmu.edu.my | 0129817286
 // *********************************************************
 // Task Distribution
